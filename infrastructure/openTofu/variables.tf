8075b4190181c4e42587aad8878b4e3fb6f7101a
Thaninux <guerninenina@gmail.com>
2025-06-18 20:03:16 +0200

    OpenTofu EC2 config

---
+++ infrastructure/openTofu/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "credentials_file" {
+  default = "../_credentials/aws_learner_lab_credentials"
+}
+
+variable "profile" {
+  default = "awslearnerlab"
+}
+
+variable "key_name" {
+  default = "vockey"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
